17321b3fcecb1dddfc7957ac78701cbad8445c81
saulwolfcode <43302778+saulwolfcode@users.noreply.github.com>
2019-03-06 08:29:00 -0300

    Se llamo la funcion todo ok.

--- src/app/Index.js
+++ src/app/Index.js
@@ -10,11 +10,10 @@ import style from "../main.css"
 
 const ui = new UI()
 const weather = new Weather("London", "uk")
-
+fetchWeather()
 async function fetchWeather() {
     const data = await weather.getWeather()
     console.log(data)
     ui.render(data)
 }
-
 document.addEventListener("DOMContentLoader", fetchWeather)
